7fb9acbb7e9eb007549e73809c1c885e92bf57bc
Lucas Kruk <lucaskruk13@gmail.com>
2017-02-23 07:52:36 -0600

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,39 +1,31 @@
-/**
- * Created by skywalker on 2/21/17.
- */
-
 $(document).ready(function(){
-
-    createGrid(50);
-
+  createTable();
 });
 
-function createGrid(width) {
-    width = width || 16;
-
-    var cellWidth = 100.0 / width;
-    var cellHeight = 500 / width;
-
-
-
-    console.log(cellWidth);
-    console.log(cellHeight);
-    //Create the table
-    $('.container').append("<table id='gridTable' cellspacing='0' cellpadding='0'></table>");
-
-    //Create each row
-    for (var i = 0; i<width; i++)
-        $('#gridTable').append("<tr class='gridRow' id='row" + i + "' style='height: "+ cellHeight +"px'></tr>");
-
-    //create each cell
-        for (var i = 0; i<width; i++){
-
-            for (var j=0; j<width; j++)
-                $("#row"+ i).append("<td style='width: " + cellWidth + "%' class='cell'></td>");
-        }
-
+function createTable(width){
+  width = width || 16;
+  
+  var cellHeight = 500.0 / width;
+  
+  
+  for (i = 0; i< width; i++){
+    $('#container').append("<div class='cellRow' id='cellRow"+ i+"' style='height:" + cellHeight + "px' ></div>");
+    
+    for (j=0; j< width; j++){
+      $('#cellRow' + i).append("<div class='cell' id='cell" + i + "-" + j + "' style='width:"+cellHeight+"px; height:"+cellHeight+"px'></div>");
+    }
+  }
 }
 
-$(document).on("mouseenter", ".cell", function(){
-    $(this).addClass("cellTouched");
-})
+$(document).on('mouseenter', '.cell', function(){
+  $(this).addClass("cellTouched");
+}
+);
+
+$('button').click(function(){
+ var newSize = window.prompt("Enter a new board size", "Default is 16");
+ 
+ $(".cellRow").remove();
+ 
+ createTable(newSize);
+});
